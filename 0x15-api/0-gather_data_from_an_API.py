14a2b58ccf0cf9039ea4142817d38b8609fdf9c3
Cemordi <emordichukwuma@gmail.com>
2023-02-09 12:57:18 +0000

    executable

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,30 +1,21 @@
 #!/usr/bin/python3
-"""Accessing a REST API for todo lists of employees"""
+"""getting data from an api
+"""
 
 import requests
 from sys import argv
 
-
 if __name__ == '__main__':
-    us_id = argv[1]
-    url = "https://jsonplaceholder.typicode.com/"
-    url_user = url + "users/{}".format(us_id)
-
-    users = requests.get(url_user).json()
-    employeeName = users.get('name')
-
-    todoUrl = url + "todos"
-    todos  = requests.get(todoUrl).json()
-    done = 0
-    completed = []
-
-    for task in todos:
-        if task.get('completed') is True:
-            completed.append(task)
-            done += 1
-
-    print("Employee {} is done with tasks({}/{}):"
-          .format(employeeName, len(completed), len(todos)))
-
-    for task in completed:
-        print("\t {}".format(task.get('title')))
+        endpoint = "https://jsonplaceholder.typicode.com/"
+            userId = argv[1]
+                user = requests.get(endpoint + "users/{}".
+                                                format(userId)).json()
+                    todo = requests.get(endpoint + "todos?userId={}".
+                                                    format(userId)).json()
+                        completed_tasks = []
+                            for task in todo:
+                                        if task.get('completed') is True:
+                                                        completed_tasks.append(task.get('title'))
+                                                            print("Employee {} is done with tasks({}/{}):".
+                                                                              format(user.get('name'), len(completed_tasks), len(todo)))
+                                                                print("\n".join("\t {}".format(task) for task in completed_tasks))
