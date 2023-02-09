08f84b68b0884d0ce83788318e1ec6a6d4c8aa74
Cemordi2 <emordichukwuma@gmail.com>
2023-02-09 14:46:04 +0100

    Api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,21 +1,31 @@
 #!/usr/bin/python3
-"""getting data from an api
-"""
+"""Accessing a REST API for todo lists of employees"""
 
 import requests
-from sys import argv
+import sys
+
 
 if __name__ == '__main__':
-        endpoint = "https://jsonplaceholder.typicode.com/"
-            userId = argv[1]
-                user = requests.get(endpoint + "users/{}".
-                                                format(userId)).json()
-                    todo = requests.get(endpoint + "todos?userId={}".
-                                                    format(userId)).json()
-                        completed_tasks = []
-                            for task in todo:
-                                        if task.get('completed') is True:
-                                                        completed_tasks.append(task.get('title'))
-                                                            print("Employee {} is done with tasks({}/{}):".
-                                                                              format(user.get('name'), len(completed_tasks), len(todo)))
-                                                                print("\n".join("\t {}".format(task) for task in completed_tasks))
+    employeeId = sys.argv[1]
+    baseUrl = "https://jsonplaceholder.typicode.com/users"
+    url = baseUrl + "/" + employeeId
+
+    response = requests.get(url)
+    employeeName = response.json().get('name')
+
+    todoUrl = url + "/todos"
+    response = requests.get(todoUrl)
+    tasks = response.json()
+    done = 0
+    done_tasks = []
+
+    for task in tasks:
+        if task.get('completed'):
+            done_tasks.append(task)
+            done += 1
+
+    print("Employee {} is done with tasks({}/{}):"
+          .format(employeeName, done, len(tasks)))
+
+    for task in done_tasks:
+        print("\t {}".format(task.get('title')))
