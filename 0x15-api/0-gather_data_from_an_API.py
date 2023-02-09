de7ce20f92b00bd9020a206574cb4c103a9c9575
Cemordi <emordichukwuma@gmail.com>
2023-02-09 12:30:35 +0000

    executable

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,13 +2,13 @@
 """Accessing a REST API for todo lists of employees"""
 
 import requests
-import sys
+from sys import argv
 
 
 if __name__ == '__main__':
-    employeeId = sys.argv[1]
+    us_id = argv[1]
     url = "https://jsonplaceholder.typicode.com/"
-    url_user = url + "users/" + "employeeId"
+    url_user = url + "users/{}".format(us_id)
 
     users = requests.get(url_user).json()
     employeeName = users.get('name')
@@ -26,5 +26,5 @@ if __name__ == '__main__':
     print("Employee {} is done with tasks({}/{}):"
           .format(employeeName, done, len(todos)))
 
-    for task in done_tasks:
+    for task in completed:
         print("\t {}".format(task.get('title')))
